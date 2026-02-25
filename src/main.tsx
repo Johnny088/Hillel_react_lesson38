e6c3c6247d098fe67a01e9b3ab375b022620945c
Johnny <esp25goodlife@gmail.com>
2026-02-25 10:35:34 +0200

    done

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { createRoot } from 'react-dom/client';
+import { students } from './data';
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const markUp = (
+  <ul>
+    {students.map(student => (
+      <li key={student.id}>
+        <p>{student.name}</p>
+        <p>{student.age}</p>
+        <p>{student.course}</p>
+        <p>{student.isOnline}</p>
+        <img src={student.avatar} />
+      </li>
+    ))}
+  </ul>
+);
+createRoot(document.getElementById('root') as HTMLDivElement).render(markUp);
